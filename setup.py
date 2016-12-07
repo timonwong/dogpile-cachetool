7ea447a47d47cfecbfd8ae45bfd614a86ea27059
Timon Wong <timon86.wang@gmail.com>
2016-12-07 15:53:14 +0800

    Fix setup.py classifier

--- setup.py
+++ setup.py
@@ -32,8 +32,9 @@ setuptools.setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
     ],
     install_requires=[
         'six',
